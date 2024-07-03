ea537a3ca17b5fe583d673221792247ba99583d3
Andrew Bulford <andrew.bulford@form3.tech>
2024-07-03 11:31:08 +0100

    fix: Support images with digests

--- bridge/bridge_test.go
+++ bridge/bridge_test.go
@@ -21,3 +21,32 @@ func TestNewValid(t *testing.T) {
 	assert.NotNil(t, bridge)
 	assert.NoError(t, err)
 }
+
+func Test_getServiceNameFromImage(t *testing.T) {
+	tests := []struct {
+		name    string
+		image   string
+		service string
+	}{
+		{
+			name:    "with tag",
+			image:   "123456789012.dkr.ecr.eu-west-1.amazonaws.com/foo/bar:v1.2.3",
+			service: "bar",
+		},
+		{
+			name:    "with digest",
+			image:   "123456789012.dkr.ecr.eu-west-1.amazonaws.com/foo/bar@sha256:abcc3e12da56ecf5756b48502baa2a98d09e69c5d033dc37ce93de77dc6cb123",
+			service: "bar",
+		},
+		{
+			name:    "with both",
+			image:   "123456789012.dkr.ecr.eu-west-1.amazonaws.com/foo/bar:v1.2.3@sha256:abcc3e12da56ecf5756b48502baa2a98d09e69c5d033dc37ce93de77dc6cb123",
+			service: "bar",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.service, getServiceNameFromImage(tt.image), "getServiceNameFromImage(%v)", tt.image)
+		})
+	}
+}
